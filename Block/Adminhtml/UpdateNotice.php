77f1ec12b3588fcc92beecf3f5b0ad3288763803
eTechFlow CI <ci@etechflow.com>
2026-06-27 11:03:57 +0000

    feat: v1.2.0 - add update-notification (banner+bell) + module-status display (parity with other modules)

---
+++ Block/Adminhtml/UpdateNotice.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+namespace Etechflow\CanonicalHreflang\Block\Adminhtml;
+
+use Etechflow\CanonicalHreflang\Model\UpdateChecker;
+use Magento\Backend\Block\Template;
+use Magento\Backend\Block\Template\Context;
+
+class UpdateNotice extends Template
+{
+    public function __construct(
+        Context $context,
+        private readonly UpdateChecker $updateChecker,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+    }
+
+    public function getUpdateInfo(): ?array
+    {
+        return $this->updateChecker->getAvailableUpdate();
+    }
+
+    public function getUpdateCommand(): string
+    {
+        return $this->updateChecker->getUpdateCommand();
+    }
+}
